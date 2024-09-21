293a000886cfbf06faac81b7ab0b55205ebefe2b
Mamdouh Khalaf <100497009+mamdou75alaf@users.noreply.github.com>
2024-09-22 02:23:42 +0300

    PWA

--- service-worker.js
+++ service-worker.js
@@ -1,20 +1,28 @@
-self.addEventListener('install', (event) => {
-  event.waitUntil(
-    caches.open('my-pwa-cache').then((cache) => {
-      return cache.addAll([
-		"index.html",
-		"icon.png",
-		"manifest.webmanifest",
-		"style.css",
-        // Add other assets and routes to cache
-      ]);
-    })
-  );
+{
+    if ("serviceWorker" in navigator) {
+        window.addEventListener("load", () => {
+            navigator.serviceWorker
+                .register("service-worker.js")
+                .then((registration) => {
+                    console.log("Service Worker registered with scope:", registration.scope);
+                })
+                .catch((error) => {
+                    console.log("Service Worker registration failed:", error);
+                });
+        });
+    }
+}
+self.addEventListener("install", (event) => {
+    event.waitUntil(
+        caches.open("my-pwa-cache").then((cache) => {
+            return cache.addAll(["icon.png", "index.html", "manifest.webmanifest", "pulltorefresh.js", "style.css"]);
+        })
+    );
 });
-self.addEventListener('fetch', (event) => {
-  event.respondWith(
-    caches.match(event.request).then((response) => {
-      return response || fetch(event.request);
-    })
-  );
+self.addEventListener("fetch", (event) => {
+    event.respondWith(
+        caches.match(event.request).then((response) => {
+            return response || fetch(event.request);
+        })
+    );
 });
