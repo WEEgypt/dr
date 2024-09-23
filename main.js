7943506a3025ca97dbd0b7caecdeb6161fadb605
Mamdouh Khalaf <100497009+mamdou75alaf@users.noreply.github.com>
2024-09-23 15:38:52 +0300

    Update main.js

--- main.js
+++ main.js
@@ -1,7 +1,7 @@
 if ("serviceWorker" in navigator) {
     window.addEventListener("load", () => {
         navigator.serviceWorker
-            .register("sw.js", { scope: "/" })
+            .register("sw.js", { scope: "./" })
             .then((registration) => {
                 console.log("[CHECK]: Service_Worker_Registered_With_Scope", registration.scope);
             })
