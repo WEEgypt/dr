20289281f6c1a80478936610695d7244639ef1e8
Mamdouh Khalaf <100497009+mamdou75alaf@users.noreply.github.com>
2024-10-18 19:48:49 +0300

    bug free

--- main.js
+++ main.js
@@ -8,8 +8,10 @@ if ("serviceWorker" in navigator) {
           registration.scope
         );
       })
-      .catch((error) => {
-        console.log("Service Worker registration failed:", error);
+      .catch((err) => {
+        console.log("Service Worker registration failed:", err);
       });
   });
+} else {
+  console.log("Service Workers are not supported in this browser.");
 }
